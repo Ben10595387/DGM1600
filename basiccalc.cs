2d749833c500700830f0a697dca64c381b9e038b
Ben10595387 <10595387@my.uvu.edu>
2017-04-20 12:10:11 -0600

    Add files via upload
    Basic Calculator and button array scripts and scenes.

---
+++ basiccalc.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class basiccalc : MonoBehaviour {
+
+
+		public InputField firstnumber;
+		public InputField secondnumber;
+		public Text output;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+
+	public void Proses () {
+
+		int variabelSementara;
+
+		variabelSementara = int.Parse(firstnumber.text) + int.Parse(secondnumber.text);
+
+		output.text = variabelSementara.ToString();
+	}
+}
---
+++ buttonarray.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonarray : MonoBehaviour {
+	public int[] clickarray;
+
+	
+	public void addarray() {
+	 int[] temp = new int[clickarray.Length + 1];
+	 clickarray.CopyTo(temp, 0);
+	 clickarray = temp;
+	 clickarray[clickarray.Length - 1] = clickarray.Length - 1;
+		
+	}
+}
+	
+	
+	
+	
+	
+
+	
+
